b383a22a68a69d789c2caca60c680fa2db8a8b8a
Kyle <kbarber1998@gmail.com>
2020-06-18 22:06:19 -0700

    Added ULib Notify Permission

--- lua/autorun/kac_load.lua
+++ lua/autorun/kac_load.lua
@@ -5,37 +5,50 @@ KACSettings = {
     rope = {
     	update = 2, 
     	threshold = 5, 
-    	requireTrace = false
+    	requireTrace = false,
+    	isTool = true
     },
     stacker = {
     	update = 3, 
     	threshold = 6, 
-    	requireTrace = true
+    	requireTrace = true,
+    	isTool = true
     },
     stacker_improved = {
     	update = 3, 
     	threshold = 6, 
-    	requireTrace = true
+    	requireTrace = true,
+    	isTool = true
     },
     light = {
     	update = 1, 
     	threshold = 5, 
-    	requireTrace = false
+    	requireTrace = false,
+    	isTool = true
     },
     lamp = {
     	update = 1, 
     	threshold = 5, 
-    	requireTrace = false
+    	requireTrace = false,
+    	isTool = true
     },
     fading_door = {
     	update = 3, 
     	threshold = 5, 
-    	requireTrace = true
+    	requireTrace = true,
+    	isTool = true
     },
     balloon = {
     	update = 2, 
     	threshold = 5, 
-    	requireTrace = true
+    	requireTrace = true,
+    	isTool = true
+    },
+    collisions = {
+    	update = 3, 
+    	threshold = 5, 
+    	requireTrace = false,
+    	isTool = false
     }
 }
 
@@ -44,6 +57,10 @@ KACSettings = {
 if SERVER then
 	include("kac/sv_kac.lua")
 	AddCSLuaFile("kac/cl_kac.lua")
+
+	if ULib ~= nil then
+		ULib.ucl.registerAccess("kac_notify", {"admin", "superadmin"}, "Allows users to see KAC notifications", "")
+	end
 elseif CLIENT then
 	include("kac/cl_kac.lua")
 end
