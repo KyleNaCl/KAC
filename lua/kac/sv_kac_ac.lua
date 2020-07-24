d8ccb8653fa00cecdd116d5336c43e04f2d32b95
Kyle <kbarber1998@gmail.com>
2020-07-23 21:29:43 -0700

    Complete AC Overhaul
    Fixed BHop Detection Bug

--- lua/kac/sv_kac_ac.lua
+++ lua/kac/sv_kac_ac.lua
@@ -1,9 +1,78 @@
 
 print("[KAC] \tLoaded sv_kac_ac.lua")
 
-local BHopMaxCVAR = CreateConVar("sv_kac_bhop_max", 10, 128, "max amount of triggers until autoban for bhop scripts", 1 , 30)
-local BHopNotifyIntervalCVAR = CreateConVar("sv_kac_bhop_notify_interval", 3, 128, "amount of triggers until staff get a notify", 1 , 10)
-local SilentIntervalCVAR = CreateConVar("sv_kac_silent_interval", 4, 128, "amount of engine ticks to consider silent aim", 0 , 10)
+KAC.Triggers = {
+    aimbot = {
+        threshold = 10,
+        delta = 0.05,
+        desc = "Aimbot"
+    },
+    bhop = {
+        threshold = 20,
+        delta = 0.05,
+        desc = "BHOP Scripts"
+    },
+    autoshoot = {
+        threshold = -1,
+        delta = 1,
+        desc = "Autoshoot"
+    }
+}
+
+local function checkType(ply, type_)
+    local steamC = KAC.checkData(ply)
+    if steamC then
+        if not steamC then KAC.print2("[KAC] Error: steamC is null in checkType()") return end
+        if not type_ then KAC.print2("[KAC] Error: type_ is null in checkType()") return end
+        if KAC.Triggers[type_][steamC] == nil then return end
+        if KAC.Triggers[type_][steamC] > 0 then 
+            KAC.Triggers[type_][steamC] = KAC.Triggers[type_][steamC] - KAC.Triggers[type_]["delta"]
+            if KAC.Triggers[type_][steamC] <= 0 then 
+                KAC.Triggers[type_][steamC] = 0
+                if KAC.Triggers[type_].threshold != -1 then
+                    KAC.print2("[KAC] Info: " .. ply:Name() .. "<" .. ply:SteamID() .. "> " .. type_ .. " delta zero'd")
+                end
+            end
+        end
+    end
+end
+
+local function loopDelta()
+    for k, ply in pairs(player.GetAll()) do
+        local steamC = KAC.checkData(ply)
+        if steamC then
+            checkType(ply,"aimbot")
+            checkType(ply,"bhop")
+            checkType(ply,"autoshoot")
+        end
+    end
+end
+timer.Create("KAC_TriggerDelta", 1, 0, loopDelta)
+
+local function pushTrigger(ply, type_, amount)
+    local steamC = KAC.checkData(ply)
+    if steamC then
+        if KAC.Triggers[type_][steamC] == nil then KAC.Triggers[type_][steamC] = 0 end
+        if KAC.Triggers[type_].threshold == -1 then
+            KAC.Triggers[type_][steamC] = amount
+        else
+            KAC.Triggers[type_][steamC] = KAC.Triggers[type_][steamC] + amount
+            if KAC.Triggers[type_][steamC] >= KAC.Triggers[type_].threshold then
+                KAC.print2("[KAC] Info: " .. ply:Name() .. "<" .. ply:SteamID() .. "> banning for " .. KAC.Triggers[type_].desc)
+                KAC.printClient(ply:UserID(), -3, "Anti-Cheat# Banned For " .. KAC.Triggers[type_].desc)
+                KAC.Triggers[type_][steamC] = 0
+                if not ply:IsListenServerHost() then
+                    ply:Lock()
+                    timer.Simple(1, function() 
+                        RunConsoleCommand("ulx", "ban", ply:Nick(), 0, KAC.Triggers[type_].desc .. " [Banned By: KAC]")
+                    end)
+                end
+            else
+                KAC.print2("[KAC] Info: " .. ply:Name() .. "<" .. ply:SteamID() .. "> " .. type_ .. " [" .. KAC.Triggers[type_][steamC] .. " / " .. KAC.Triggers[type_].threshold .. "]")
+            end
+        end
+    end
+end
 
 timer.Simple(5,function() -- Wait for WireLib to be mounted
 
@@ -13,6 +82,9 @@ timer.Simple(5,function() -- Wait for WireLib to be mounted
                 local steamC = KAC.checkData(ply)
                 if steamC then
                     KAC[steamC].button[binding] = 1
+                    if binding == "attack" then
+                        pushTrigger(ply, "autoshoot", 5)
+                    end
                 end
             end
         end)
@@ -29,46 +101,29 @@ timer.Simple(5,function() -- Wait for WireLib to be mounted
 
 end)
 
-local function subT(Num1, Num2)
-    if Num1 < Num2 then
-        if Num1 + 180 < Num2 then
-            return (Num1 + 180) + ((Num2 - 180) * -1)
-        end
-    else
-        if Num2 + 180 < Num1 then
-            return (Num2 + 180) + ((Num1 - 180) * -1)
-        end
-    end
-    return Num1 - Num2
+local function subT(A, B)
+    Diff = ((A - B) + 180) % 360 - 180
+    if Diff < 180 then return Diff end
+    return Diff - 360
 end
 
 hook.Add("OnPlayerHitGround", "KAC_Ground", function(ply, inWater, onFloater, speed)
     local steamC = KAC.checkData(ply)
     if not ply or inWater or onFloater then return end
-    if not ply:Alive() or ply:GetMoveType() == MOVETYPE_NOCLIP then return end
+    if not ply:Alive() or ply:GetMoveType() == MOVETYPE_NOCLIP or ply:IsTimingOut() then return end
 
     local isJump = KAC[steamC].button.jump
     if isJump == 1 then
-        timer.Simple(0, function() 
+        timer.Simple(0, function()
+            if ply:IsOnGround() or not ply:Alive() or ply:IsTimingOut() then return end
+            if KAC[steamC].button.jump == -1 or ply:GetMoveType() == MOVETYPE_NOCLIP then return end
+
             local vel = ply:GetVelocity()
-            if vel[3] <= 0 or KAC[steamC].button.jump == -1 then return end
+            if math.abs(math.Round(vel[1]) + math.Round(vel[2])) <= 300 or math.Round(vel[3]) < 50 then return end
 
-            KAC.print2("[KAC] Info: " .. ply:Name() .. "<" .. ply:SteamID() .. "> bhop detected [x " .. math.Round(vel[1]) .. ", y " .. math.Round(vel[2]) .."]")
-            KAC[steamC].bhop = KAC[steamC].bhop + 1
-
-            if KAC[steamC].bhop >= BHopMaxCVAR:GetInt() then
-                KAC.print2("[KAC] Info: " .. ply:Name() .. "<" .. ply:SteamID() .. "> banning for bhop scripts [" .. KAC[steamC].bhop .. "]")
-                KAC.printClient(ply:UserID(), -3, "Anti-Cheat# BHOP Scripts")
-                ply:Lock()
-                KAC[steamC].bhop = 0
-                timer.Simple(1, function() 
-                    RunConsoleCommand("ulx", "ban", ply:Nick(), 0, "BHOP Scripts [Banned By: KAC]")
-                end)
-            else
-                if KAC[steamC].bhop % BHopNotifyIntervalCVAR:GetInt() == 0 then
-                    KAC.printClient(ply:UserID(), -1, "Anti-Cheat# Detected BHop Scripts")
-                end
-            end
+            //KAC.print2("[KAC] Info: " .. ply:Name() .. "<" .. ply:SteamID() .. "> bhop detected [x " .. math.Round(vel[1]) .. ", y " .. math.Round(vel[2]) .."]")
+            KAC.printClient(ply:UserID(), -1, "Anti-Cheat# Detected BHOP Scripts")
+            pushTrigger(ply, "bhop", 1)
         end)
     end
 end)
@@ -76,55 +131,50 @@ end)
 hook.Add("Tick","KAC_Tick",function()
     for k, ply in pairs(player.GetAll()) do
         local steamC = KAC.checkData(ply)
-        if steamC then
+        if KAC[steamC] then
             KAC[steamC].eyea = ply:EyeAngles()
-            if KAC[steamC].button["attack"] == 1 then
-                KAC[steamC].button["attackpre"] = CurTime()
-            end
         end
     end
 end)
 
-util.AddNetworkString("KAC_Debug_Print")
-
-local function paint(ply, vec)
-    net.Start("KAC_Debug_Print")
-        net.WriteVector(vec)
-    net.Send(ply)
-end
-
 hook.Add("EntityTakeDamage", "KAC_Damage", function(ent, dmg)
 
-    local ply = dmg:GetAttacker()
-
     if not dmg:IsBulletDamage() then return end
+    local ply = dmg:GetAttacker()
     if not IsValid(ply) or not IsValid(ent) then return end
     if not ply:IsPlayer() or not ent:IsPlayer() then return end
-    if not ent:Alive() or not ply:Alive() then return end
+    if not ply:Alive() or not ent:Alive() then return end
     if ply:IsTimingOut() or ent:IsTimingOut() then return end
     if ply:InVehicle() or ent:InVehicle() then return end
     if ply:HasGodMode() or ent:HasGodMode() then return end
     if KAC.InBuild(ply) or KAC.InBuild(ent) then return end
-
+    
     local steamC = KAC.checkData(ply)
 
     if steamC then
+
+        if KAC[steamC].antispam == CurTime() then return end
+
+        KAC[steamC].antispam = CurTime()
+
         local Min, Max = ent:GetCollisionBounds()
         local H = Max[3] - Min[3]
-        --local infl = dmg:GetInflictor()
-        --if infl:IsPlayer() then
-        --    infl = infl:GetActiveWeapon()
-        --    if infl:IsWeapon() then
-        --        if KAC[steamC].button["attack"] == -1 and CurTime() - KAC[steamC].button["attackpre"] > engine.TickInterval() * 10 then
-        --            KAC.printClient(ply:UserID(), -1, "Anti-Cheat# Auto Shoot Detected")
-        --        end
-        --    end
-        --end
+        local infl = dmg:GetInflictor()
+        if infl:IsPlayer() then
+            infl = infl:GetActiveWeapon()
+            if infl:IsWeapon() then
+                if KAC[steamC].button["attack"] == -1 and KAC.Triggers["autoshoot"][steamC] == 0 then
+                    KAC.printClient(ply:UserID(), -1, "Anti-Cheat# Detected Autoshoot")
+                    //pushTrigger(ply, "aimbot", 1)
+                end
+            end
+        end
 
         local PAng = KAC[steamC].eyea
+        local NewPAng = ply:EyeAngles()
 
-        local P = math.abs(math.Round(subT(ply:EyeAngles().p,PAng.p),2))
-        local Y = math.abs(math.Round(subT(ply:EyeAngles().y,PAng.y),2))
+        local P = math.abs(math.Round(subT(PAng.p,NewPAng.p),2))
+        local Y = math.abs(math.Round(subT(PAng.y,NewPAng.y),2))
 
         local Start = ply:GetShootPos()
         local End = dmg:GetDamagePosition()
@@ -132,11 +182,8 @@ hook.Add("EntityTakeDamage", "KAC_Damage", function(ent, dmg)
         local Dir = (End - Start):GetNormalized()
 
         if Dist > 150 then
-            --local Range = 10000 / Dist
-            local Range = math.Clamp(H / (Dist / 300),5,180)
-
-            --print(Dist .. " | " .. Range)
-
+            local Range = math.Clamp(H / (Dist / 200),1.5,180)
+            
             local Trace = ply:GetEyeTrace()
             local BTrace = util.TraceLine({start = End + (Dir * -10), endpos = End + (Dir * 10), filter = ply, ignoreworld = true})
             --local WTrace = util.TraceLine({start = Start, endpos = End + (Dir * 10), filter = ply})
@@ -155,21 +202,31 @@ hook.Add("EntityTakeDamage", "KAC_Damage", function(ent, dmg)
             KAC[steamC].hitshot = KAC[steamC].hitshot + 1
             if BTrace.HitGroup == HITGROUP_HEAD then
                 KAC[steamC].headshot = KAC[steamC].headshot + 1
-                --ply:ChatPrint("Headshot")
             end
 
             if P > Range or Y > Range then
 
-                timer.Simple(engine.TickInterval() * SilentIntervalCVAR:GetInt(),function()
-                    local P2 = math.abs(math.Round(subT(ply:EyeAngles().p,PAng.p),2))
-                    local Y2 = math.abs(math.Round(subT(ply:EyeAngles().y,PAng.y),2))
+                timer.Simple(engine.TickInterval() * 4,function()
+                    local TimerPAng = ply:EyeAngles()
 
-                    if P2 < 1 and Y2 < 1 then
-                        KAC.print2("[KAC] Info: Silent Aim Detected")
-                        KAC.printClient(ply:UserID(), -1, "Anti-Cheat# Silent Aim Detected")
+                    local PC = math.abs(math.Round(subT(TimerPAng.p,PAng.p))) < (P / 10)
+                    local YC = math.abs(math.Round(subT(TimerPAng.y,PAng.y))) < (Y / 10)
+
+                    if PC and YC then
+                        KAC.print2("[KAC] Info: Detected Silent Aim")
+                        KAC.printClient(ply:UserID(), -1, "Anti-Cheat# Detected Silent Aim")
+                        pushTrigger(ply, "aimbot", 3)
                     end
                 end)
-                KAC.printClient(ply:UserID(), -1, "Anti-Cheat# Snap Detected d[" .. math.Round(Dist * 0.0625) .. "] p[" .. math.Round(P) .. "] y[" .. math.Round(Y) .. "]")
+                local Clamp = math.Clamp(math.floor((math.max(P,Y) + (Dist / 80)) / 25), 0, 4)
+                local Text = "Low"
+                if Clamp == 2 then Text = "Medium" end
+                if Clamp == 3 then Text = "High" end
+                if Clamp == 4 then Text = "Severe" end
+                //print("Range: " .. Range .. " | M: " .. math.max(P,Y) .. " | Div: " .. ((math.max(P,Y) + (Dist / 60)) / 30))
+                //KAC.printClient(ply:UserID(), -1, "Anti-Cheat# Snap Detected d[" .. math.Round(Dist * 0.0625) .. "] p[" .. math.Round(P) .. "] y[" .. math.Round(Y) .. "]")
+                KAC.printClient(ply:UserID(), -1, "Anti-Cheat# Detected Snap Severity: " .. Text)
+                pushTrigger(ply, "aimbot", Clamp)
             end
         end
     end
