438b038bf968171a2c34112316e7d939358fce34
Kyle <kbarber1998@gmail.com>
2020-08-01 21:52:02 -0700

    Update cl_kac.lua

--- lua/kac/cl_kac.lua
+++ lua/kac/cl_kac.lua
@@ -50,3 +50,75 @@ net.Receive("KAC_Client",function()
         end
     end
 end)
+
+timer.Simple(2,function() -- Wait for WireLib to be mounted
+
+    if not FindMetaTable("Player").isBuild then
+
+        local Killfeed = {}
+
+        local function push(Str,Team1,Team2)
+            local S = string.Explode("%KAC%", Str)
+            table.insert(Killfeed,table.Count(Killfeed) + 1,{
+                A = S[1],
+                T1 = Team1,
+                W = "[" .. S[2] .. "]",
+                B = S[3],
+                T2 = Team2,
+                State = 1,
+                Life = 60,
+                X = 0
+            })
+        end
+        local function remove()
+            table.remove(Killfeed,1)
+        end
+
+        net.Receive("KAC_Killfeed",function()
+            local Str = net.ReadString()
+            local E1 = net.ReadEntity()
+            local E2 = net.ReadEntity()
+            local T1 = 1001
+            local T2 = 1001
+            if E1:IsPlayer() then T1 = E1:Team() end
+            if E2:IsPlayer() then T2 = E2:Team() end
+            push(Str,T1,T2)
+        end)
+
+        hook.Add("DrawDeathNotice", "KAC_HideOldKillfeed", function(x,y)
+            return false
+        end)
+
+        local FHeight = draw.GetFontHeight("ChatFont")
+        local FWidth = FHeight * 0.55
+        local StartY = ScrH() - 200
+
+        local function gWidth(str)
+            return string.len(str) * FWidth
+        end
+
+        hook.Add("HUDPaint", "KAC_Killfeed_Draw", function()
+            if Killfeed then
+                for k, tab in pairs(Killfeed) do
+                    if tab != nil then
+                        local Offset = (string.len(tab["B"]) * FWidth) + 50
+                        draw.SimpleTextOutlined(tab["A"], "ChatFont", ScrW() + 250 - (tab["X"] + 20 + gWidth(tab["W"]) + gWidth(tab["B"])), StartY - FHeight * k, team.GetColor(tab["T1"]), TEXT_ALIGN_RIGHT, TEXT_ALIGN_RIGHT, 0.75, Color(0,0,0,150))
+                        draw.SimpleTextOutlined(tab["W"], "ChatFont", ScrW() + 250 - (tab["X"] + 10 + gWidth(tab["B"])), StartY - FHeight * k, Color(240,240,240,255), TEXT_ALIGN_RIGHT, TEXT_ALIGN_RIGHT, 0.75, Color(0,0,0,150))
+                        draw.SimpleTextOutlined(tab["B"], "ChatFont", ScrW() + 250 - (tab["X"]), StartY - FHeight * k, team.GetColor(tab["T2"]), TEXT_ALIGN_RIGHT, TEXT_ALIGN_RIGHT, 0.75, Color(0,0,0,150))
+                        if tab["State"] == 1 then
+                            if tab["X"] < 300 then tab["X"] = tab["X"] + 20 end
+                            if tab["X"] >= 300 then tab["X"] = 300 tab["State"] = 2 end
+                        elseif tab["State"] == 2 then
+                            tab["Life"] = tab["Life"] - 0.1
+                            if tab["Life"] <= 1 then tab["Life"] = 1 tab["State"] = 3 end
+                        elseif tab["State"] == 3 then
+                            tab["X"] = tab["X"] - 15
+                            if tab["X"] <= -400 then tab["State"] = 4 remove() end
+                        end
+                    end
+                end
+            end
+        end)
+    end
+
+end
