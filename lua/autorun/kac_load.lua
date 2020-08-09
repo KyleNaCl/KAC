683edb9618c1cb1826167718e9774f990993ac9a
Kyle <kbarber1998@gmail.com>
2020-08-09 11:35:50 -0700

    Update kac_load.lua
    Added Options For Custom Features
    Added Function to Reload KAC

--- lua/autorun/kac_load.lua
+++ lua/autorun/kac_load.lua
@@ -1,70 +1,80 @@
 
 -----Settings-----
 
-KACSettings = {
-    rope = {
-    	update = 2, 
-    	threshold = 5, 
-    	requireTrace = false,
-    	isTool = true
-    },
-    stacker = {
-    	update = 3, 
-    	threshold = 6, 
-    	requireTrace = true,
-    	isTool = true
-    },
-    stacker_improved = {
-    	update = 3, 
-    	threshold = 6, 
-    	requireTrace = true,
-    	isTool = true
-    },
-    light = {
-    	update = 1, 
-    	threshold = 5, 
-    	requireTrace = false,
-    	isTool = true
-    },
-    lamp = {
-    	update = 1, 
-    	threshold = 5, 
-    	requireTrace = false,
-    	isTool = true
-    },
-    fading_door = {
-    	update = 3, 
-    	threshold = 5, 
-    	requireTrace = true,
-    	isTool = true
-    },
-    balloon = {
-    	update = 2, 
-    	threshold = 5, 
-    	requireTrace = true,
-    	isTool = true
-    },
-    collisions = {
-    	update = 1, 
-    	threshold = 5, 
-    	requireTrace = false,
-    	isTool = false
-    }
-}
+function KACReload()
+	KACSettings = {
+	    rope = {
+	    	update = 2, 
+	    	threshold = 5, 
+	    	requireTrace = false,
+	    	isTool = true
+	    },
+	    stacker = {
+	    	update = 3, 
+	    	threshold = 6, 
+	    	requireTrace = true,
+	    	isTool = true
+	    },
+	    stacker_improved = {
+	    	update = 3, 
+	    	threshold = 6, 
+	    	requireTrace = true,
+	    	isTool = true
+	    },
+	    light = {
+	    	update = 1, 
+	    	threshold = 5, 
+	    	requireTrace = false,
+	    	isTool = true
+	    },
+	    lamp = {
+	    	update = 1, 
+	    	threshold = 5, 
+	    	requireTrace = false,
+	    	isTool = true
+	    },
+	    fading_door = {
+	    	update = 3, 
+	    	threshold = 5, 
+	    	requireTrace = true,
+	    	isTool = true
+	    },
+	    balloon = {
+	    	update = 2, 
+	    	threshold = 5, 
+	    	requireTrace = true,
+	    	isTool = true
+	    },
+	    collisions = {
+	    	update = 1, 
+	    	threshold = 5, 
+	    	requireTrace = false,
+	    	isTool = false
+	    },
+	    InfiniteReserves = true,
+	    CustomKillfeed = true,
+	    KACCol = Color(100,100,255),
+	    TextSep = Color(200,200,200),
+	    TextCol = Color(255,255,255)
+	}
 
------Loads-----
+	-----Loads-----
 
-if SERVER then
-	print("[KAC] ======Initalize Server======")
-	include("kac/sv_kac.lua")
-	include("kac/sv_kac_ac.lua")
-	AddCSLuaFile("kac/cl_kac.lua")
-	print("[KAC] ============================")
-	if ULib ~= nil then
-		ULib.ucl.registerAccess("kac_notify", {"admin", "superadmin"}, "Allows users to see KAC notifications", "")
+	if SERVER then
+		print("[KAC] ======Initalize Server======")
+		include("kac/sv_kac.lua")
+		include("kac/sv_kac_ac.lua")
+		print("[KAC] ============================")
+		AddCSLuaFile("kac/cl_kac.lua")
+		
+		if ULib ~= nil then
+			ULib.ucl.registerAccess("kac_notify", {"admin", "superadmin"}, "Allows users to see KAC notifications", "")
+		end
+	elseif CLIENT then
+		print("[KAC] ======Initalize Client======")
+		include("kac/cl_kac.lua")
+		print("[KAC] ============================")
 	end
-elseif CLIENT then
-	print("[KAC] ======Initalize Client======")
-	include("kac/cl_kac.lua")
-	print("[KAC] ============================")
 end
+
+KACReload()
