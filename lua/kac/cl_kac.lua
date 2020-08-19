443f22d759c50bf1d91fec3e68637e0f593d8ac5
Kyle <kbarber1998@gmail.com>
2020-08-19 16:19:30 -0700

    Fixed Lua Error on print during spawning

--- lua/kac/cl_kac.lua
+++ lua/kac/cl_kac.lua
@@ -3,6 +3,7 @@ print("[KAC] \tLoaded cl_kac.lua")
 
 local Settings = {
     CustomKillfeed = false,
+    SimpleCrosshair = false,
     KACCol = Color(255,255,255),
     TextSep = Color(200,200,200),
     TextCol = Color(255,255,255)
@@ -38,6 +39,7 @@ net.Receive("KAC_Settings",function()
     print("[KAC] \tRecieved Settings From Server")
 
     Settings.CustomKillfeed = net.ReadBool()
+    Settings.SimpleCrosshair = net.ReadBool()
     Settings.KACCol = net.ReadColor()
     Settings.TextSep = net.ReadColor()
     Settings.TextCol = net.ReadColor()
@@ -79,6 +81,11 @@ net.Receive("KAC_Settings",function()
                     end
                 end
             end
+        end)
+    end
+
+    if Settings.SimpleCrosshair == true then
+        hook.Add("HUDPaint", "KAC_Crosshair_Draw", function()
             surface.SetDrawColor(0,0,0)
             surface.DrawRect((surface.ScreenWidth() / 2) - 2,(surface.ScreenHeight() / 2) - 2,4,4)
             surface.SetDrawColor(255,255,255)
@@ -89,46 +96,49 @@ end)
 
 net.Receive("KAC_Client",function()
 
-    local Str = string.Explode("^", net.ReadString())
-    local TargetID = tonumber(Str[1])
-    local VictimID = tonumber(Str[2])
-    local Message = Str[3]
+    if IsValid(LocalPlayer()) then
+        local Str = string.Explode("^", net.ReadString())
+        local TargetID = tonumber(Str[1])
+        local VictimID = tonumber(Str[2])
+        local Message = Str[3]
 
-    if TargetID > 0 then 
-        local ply = Player(TargetID)
-        if ply then
-            local name = ply:Name()
-            if ply == LocalPlayer() then name = "You" end
+        if TargetID > 0 then 
+            local ply = Player(TargetID)
+            if IsValid(ply) then
+                local name = ply:Name()
+                if ply == LocalPlayer() then name = "You" end
+                if VictimID > 0 then 
+                    local vic = Player(VictimID)
+                    if IsValid(vic) then
+                        local vicname = vic:Name()
+                        if vic == LocalPlayer() then vicname = "You" end
+                        if not string.find(Message, "#") then Message = Message .. "#" end
+                        local StrS = string.Explode("#",Message)
+                        chat.AddText(Settings.TextSep,"[",Settings.KACCol,"KAC",Settings.TextSep,"]",Settings.TextCol," ",team.GetColor(ply:Team()),name,Settings.TextCol," " .. StrS[1] .. " ",team.GetColor(vic:Team()),vicname,Settings.TextCol," " .. StrS[2])
+                    end
+                else 
+                    if string.find(Message, "#") then
+                        local A = string.Explode("#", Message)
+                        chat.AddText(Settings.TextSep,"[",Settings.KACCol,"KAC",Settings.TextSep,"]",Settings.TextCol," ",team.GetColor(ply:Team()),name,Settings.TextCol," ",Color(255,100,100),A[1],Settings.TextCol,A[2])
+                    else
+                        chat.AddText(Settings.TextSep,"[",Settings.KACCol,"KAC",Settings.TextSep,"]",Settings.TextCol," ",team.GetColor(ply:Team()),name,Settings.TextCol," " .. Message)
+                    end
+                end
+            else
+                LocalPlayer():PrintMessage(HUD_PRINTCONSOLE, "[KAC] Error: ply not found: " .. Message)
+            end
+        else
             if VictimID > 0 then 
                 local vic = Player(VictimID)
                 if vic then
                     local vicname = vic:Name()
                     if vic == LocalPlayer() then vicname = "You" end
-                    if not string.find(Message, "#") then Message = Message .. "#" end
                     local StrS = string.Explode("#",Message)
-                    chat.AddText(Settings.TextSep,"[",Settings.KACCol,"KAC",Settings.TextSep,"]",Settings.TextCol," ",team.GetColor(ply:Team()),name,Settings.TextCol," " .. StrS[1] .. " ",team.GetColor(vic:Team()),vicname,Settings.TextCol," " .. StrS[2])
-
+                    chat.AddText(Settings.TextSep,"[",Settings.KACCol,"KAC",Settings.TextSep,"] ",Settings.TextCol,"Unknown Player " .. StrS[1] .. " ",team.GetColor(vic:Team()),vicname,Settings.TextCol," " .. StrS[2])
                 end
-            else 
-                if string.find(Message, "#") then
-                    local A = string.Explode("#", Message)
-                    chat.AddText(Settings.TextSep,"[",Settings.KACCol,"KAC",Settings.TextSep,"]",Settings.TextCol," ",team.GetColor(ply:Team()),name,Settings.TextCol," ",Color(255,100,100),A[1],Settings.TextCol,A[2])
-                else
-                    chat.AddText(Settings.TextSep,"[",Settings.KACCol,"KAC",Settings.TextSep,"]",Settings.TextCol," ",team.GetColor(ply:Team()),name,Settings.TextCol," " .. Message)
-                end
+            else
+                chat.AddText(Settings.TextSep,"[",Settings.KACCol,"KAC",Settings.TextSep,"] ",Settings.TextCol,Message)
             end
-        end
-    else
-        if VictimID > 0 then 
-            local vic = Player(VictimID)
-            if vic then
-                local vicname = vic:Name()
-                if vic == LocalPlayer() then vicname = "You" end
-                local StrS = string.Explode("#",Message)
-                chat.AddText(Settings.TextSep,"[",Settings.KACCol,"KAC",Settings.TextSep,"] ",Settings.TextCol,"Unknown Player " .. StrS[1] .. " ",team.GetColor(vic:Team()),vicname,Settings.TextCol," " .. StrS[2])
-            end
-        else
-            chat.AddText(Settings.TextSep,"[",Settings.KACCol,"KAC",Settings.TextSep,"] ",Settings.TextCol,Message)
         end
     end
 end)
